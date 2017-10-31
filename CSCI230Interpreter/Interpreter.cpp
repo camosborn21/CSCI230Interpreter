920ff3bf1996cda787754cf15905554eecc52df7
Cameron Osborn <camosborn21@msn.com>
2017-10-31 12:57:50 -0700

    Template for interpreter created

--- CSCI230Interpreter/Interpreter.cpp
+++ CSCI230Interpreter/Interpreter.cpp
@@ -1,7 +1,15 @@
 #include "stdafx.h"
 #include "Interpreter.h"
 
+//Convert source program lines to execution statements to allow multiline statements or multiple statements per line.
 
+//Template
+//	Parse lines to statements
+//	Check Syntax
+//	Execute statements 1 line at a time
+//		read
+//		display
+//		assignment
 Interpreter::Interpreter()
 {
 }
